3d4ce47c400f13551c52ca44dcbb12fe7326e9b2
Elliot Morrison <elliotmorrison58@gmail.com>
2023-09-24 23:33:50 -0500

    updated main page

--- src/routes/(auth)/main/+page.svelte
+++ src/routes/(auth)/main/+page.svelte
@@ -7,6 +7,6 @@
     <title>My Stuff</title>
 </svelte:head>
 
-<p class="text-white text-3xl font-semibold">"Welcome to Crestfallen! <br> This will be the hub of my many (relatively simple) side project/coding ideas 😊" 
+<p class="text-white text-3xl font-semibold">"Welcome to Crestfallen! <br> This will be the hub of my many random (yet relatively simple) side project/coding ideas 😊" 
    <br> - <span class="italic text-xl">Elliot Morrison</span>
 </p>
