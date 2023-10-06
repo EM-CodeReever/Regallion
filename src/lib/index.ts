fa2a8c06fc8f82bc1da3810b2c15cb2d233a9b57
Elliot Morrison <elliotmorrison58@gmail.com>
2023-10-06 09:06:08 -0500

    Tiny prisma import

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
-// import { PrismaClient } from '@prisma/client';
-// let prisma = new PrismaClient();
+import { PrismaClient } from '@prisma/client';
+let prisma = new PrismaClient();
 
 // export enum Gender {
 //     MALE= 'MALE',
@@ -8,4 +8,4 @@
 //     RNS = 'RNS'
 // }
 
-// export { prisma };
+export { prisma };
