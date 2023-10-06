c3f5b1839bdeb38accefdf2ec498bc6bad787233
Elliot Morrison <elliotmorrison58@gmail.com>
2023-10-06 09:26:53 -0500

    I've had enough

--- src/routes/(auth)/dashboard/+page.server.ts
+++ src/routes/(auth)/dashboard/+page.server.ts
@@ -1,5 +1,5 @@
 import type { PageServerLoad } from './$types';
-// import { prisma } from '$lib/index';
+import { prisma } from '$lib/index';
 
 
 export const load = (async ({locals:{getSession}}) => {
@@ -16,30 +16,24 @@ export const actions = { updateProfile: async ({ request }) => {
       const phoneNumber = formData.get('phoneNumber');
       const sessionUserId = formData.get('sessionUserId');
       const sessionUserEmail = formData.get('sessionUserEmail');
-        console.log(firstName);
         
-        let profile = firstName;
-      // let profile = await prisma.profile.create({
-      //       data: {
-      //          auth_user_id: sessionUserId as string,
-      //          auth_email: sessionUserEmail as string,
-      //          avatar: '',
-      //          created_at: new Date(),
-      //          date_of_birth: new Date(dateOfBirth as string),
-      //          first_name: firstName as string,
-      //          gender: 'RNS',
-      //          last_name: lastName as string,
-      //          username: username as string,
-      //       },
-      //   });
+      let profile = await prisma.profile.create({
+            data: {
+               auth_user_id: sessionUserId as string,
+               auth_email: sessionUserEmail as string,
+               avatar: '',
+               created_at: new Date(),
+               date_of_birth: new Date(dateOfBirth as string),
+               first_name: firstName as string,
+               gender: gender as string,
+               last_name: lastName as string,
+               username: username as string,
+            },
+        });
         if (profile) {
-            console.log(profile);
-        
             return { success: true };
         }else{
             return { success: false };
         }
-    //   console.log(formData);
-      // Process the form data and perform actions
     },
   };
