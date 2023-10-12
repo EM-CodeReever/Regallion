ae77b9492d709a0330425ebc28cf1687ea2cbfba
Elliot Morrison <elliotmorrison58@gmail.com>
2023-10-12 11:54:20 -0500

    Initial commit

---
+++ src/routes/(auth)/memory-card/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+</script>
+
+<section class="w-full wave-yellow h-screen flex justify-center items-center">
+
+</section>
