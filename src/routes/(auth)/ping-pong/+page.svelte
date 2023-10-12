94f18cf5faa37ed65935e6d375f8232e80ad245b
Elliot Morrison <elliotmorrison58@gmail.com>
2023-10-12 09:53:23 -0500

    canvas will stress me another day

--- src/routes/(auth)/ping-pong/+page.svelte
+++ src/routes/(auth)/ping-pong/+page.svelte
@@ -19,7 +19,8 @@
 	let gameEnded = false;
 	let winner = "";
 	let pointsToWin = 3;
-	let canvasWidth = 800;
+	let screenSizeWidth = 0;
+	let screenSizeHeight = 0;
 
 	function setDifficulty(type: "easy" | "hard" | "unfair") {
 		if (type === "easy") {
@@ -58,9 +59,7 @@
 				"pingPongCanvas"
 			) as HTMLCanvasElement;
 			// determine canvas size from screen size
-			const ctx = canvas.getContext(
-				"2d"
-			) as CanvasRenderingContext2D;
+			const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
 			// Paddle properties
 			const paddleWidth = 15;
@@ -82,10 +81,6 @@
 
 			// Game loop
 			function gameLoop() {
-				if (window.innerWidth < 820) {
-					canvasWidth = window.innerWidth - 20;
-					paddleHeight = 0.3 * canvas.height;
-				}
 				movePaddles();
 				moveBall();
 				drawCanvas();
@@ -98,8 +93,7 @@
 					return;
 				}
 				const mouseY =
-					event.clientY -
-					canvas.getBoundingClientRect().top;
+					event.clientY - canvas.getBoundingClientRect().top;
 				leftPaddleY = mouseY - paddleHeight / 2;
 			});
 
@@ -250,10 +244,6 @@
 				ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 				// Draw paddles
-				// ctx.fillStyle = colorChoice;
-				// ctx.fillRect(0, leftPaddleY, paddleWidth, paddleHeight);
-				// ctx.fillRect(canvas.width - paddleWidth, rightPaddleY, paddleWidth, paddleHeight);
-
 				ctx.fillStyle = playerPaddleColor; // Paddle color
 				ctx.beginPath();
 				ctx.moveTo(0, leftPaddleY);
@@ -340,86 +330,82 @@
 	<title>Ping Pong</title>
 </svelte:head>
 
+<svelte:window
+	bind:innerWidth={screenSizeWidth}
+	bind:innerHeight={screenSizeHeight}
+/>
+
 <section
 	class="flex flex-col space-y-5 justify-center items-center pt-16 relative wave-purple h-screen"
 >
 	<div
-		class="text-lg mx-auto text-center p-3 flex-col justify-center items-center hidden"
+		class="text-lg mx-auto text-center p-3 flex-col justify-center items-center ping-pong-breakpoint:tall:hidden"
 	>
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			fill="none"
-			viewBox="0 0 24 24"
-			stroke-width="1.0"
-			stroke="currentColor"
-			class="w-24 h-24"
+		Ping pong game is unavailable on screens smaller than 820px x 680px
+	</div>
+	{#if gameStarted}
+		<div
+			in:fly={{ delay: 0, duration: 500, y: 300, opacity: 0 }}
+			class="w-full max-w-3xl justify-between space-x-10 px-4 mx-5 items-center h-16 rounded-xl bg-[#ffffff79] blur-bg text-black hidden ping-pong-breakpoint:tall:flex"
 		>
-			<path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z"
-			/>
-		</svg>
-		Ping pong game is under construction, come back later
-	</div>
-	<div
-		class="w-full max-w-3xl flex justify-between space-x-10 px-10 items-center h-20 rounded-xl bg-[#ffffff79] blur-bg text-black absolute mx-auto top-28 z-50"
-	>
-		<p class="font-sans text-lg badge light info cornered w-full">
-			{userProfile?.username} - {playerScore}
-		</p>
-		{#if !paused || gameEnded}
-			<button
-				class="btn light bw"
-				on:click={() => {
-					pauseGame();
-				}}
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-6 h-6"
+			<p class="font-sans text-lg badge light info cornered w-full">
+				{userProfile?.username} - {playerScore}
+			</p>
+			{#if !paused || gameEnded}
+				<button
+					class="btn light bw"
+					on:click={() => {
+						pauseGame();
+					}}
 				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M15.75 5.25v13.5m-7.5-13.5v13.5"
-					/>
-				</svg>
-			</button>
-		{:else if paused && !gameEnded}
-			<button
-				class="btn light bw"
-				on:click={() => {
-					resumeGame();
-				}}
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-6 h-6"
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-6 h-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M15.75 5.25v13.5m-7.5-13.5v13.5"
+						/>
+					</svg>
+				</button>
+			{:else if paused && !gameEnded}
+				<button
+					class="btn light bw"
+					on:click={() => {
+						resumeGame();
+					}}
 				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
-					/>
-				</svg>
-			</button>
-		{/if}
-		<p class="font-sans text-lg badge light warn cornered w-full">
-			Computer - {computerScore}
-		</p>
-	</div>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-6 h-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+						/>
+					</svg>
+				</button>
+			{/if}
+			<p class="font-sans text-lg badge light warn cornered w-full">
+				Computer - {computerScore}
+			</p>
+		</div>
+	{/if}
 	<canvas
 		id="pingPongCanvas"
-		class="rounded-xl canvas-aspect-ratio relative mt-36"
+		class="rounded-xl relative mt-36 hidden ping-pong-breakpoint:tall:flex"
+		height="500"
+		width="800"
 	/>
 
 	{#if scoreDisplay && !gameEnded}
@@ -430,7 +416,7 @@
 				y: 300,
 				opacity: 0,
 			}}
-			class="z-50 text-7xl text-center font-bold flex justify-center items-center absolute mx-auto my-auto"
+			class="z-50 text-7xl text-center font-bold justify-center items-center absolute mx-auto my-auto hidden ping-pong-breakpoint:tall:flex"
 		>
 			{playerScore} - {computerScore}
 		</div>
@@ -438,7 +424,7 @@
 	{#if !gameStarted}
 		<div
 			in:fly={{ delay: 0, duration: 500, y: 300, opacity: 0 }}
-			class="z-50 text-center font-bold flex flex-col space-y-5 justify-center items-center absolute mx-auto my-auto"
+			class="z-50 text-center font-bold hidden flex-col space-y-5 justify-center items-center absolute mx-auto my-auto ping-pong-breakpoint:tall:flex"
 		>
 			<h1 class="text-7xl text-center font-bold">PING PONG!</h1>
 			<span class="text-center font-bold"
@@ -463,7 +449,7 @@
 	{#if gameEnded}
 		<div
 			in:fly={{ delay: 0, duration: 500, y: 300, opacity: 0 }}
-			class="z-50 text-7xl text-center font-bold flex flex-col space-y-5 justify-center items-center absolute mx-auto my-auto"
+			class="z-50 text-7xl text-center font-bold flex-col space-y-5 justify-center items-center absolute mx-auto my-auto hidden ping-pong-breakpoint:tall:flex"
 		>
 			{winner} Wins! <br />
 			{playerScore} - {computerScore}
