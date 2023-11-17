2410da8ffbbd12cae293c08178c9eb1f2dab6ca3
Elliot Morrison <elliotmorrison58@gmail.com>
2023-11-17 10:45:07 -0500

    Added styles and foundation

--- src/routes/(auth)/memory-card/+page.svelte
+++ src/routes/(auth)/memory-card/+page.svelte
@@ -4,10 +4,29 @@
     export let data: PageData;
 </script>
 
+<svelte:head>
+    <title>Memory Cards</title>
+</svelte:head>
 <section class="w-full dusk-waves-inverse h-screen flex justify-center items-center">
-    <div class="text-lg mx-auto text-center p-3 flex flex-col justify-center items-center">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.0" stroke="currentColor" class="w-24 h-24">
+    <div class="text-lg mx-auto text-center p-3 flex flex-col justify-center items-center space-y-4">
+        <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.0" stroke="currentColor" class="w-24 h-24">
             <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
           </svg>          
-        Memory-Cards is in development</div>
+        Memory-Cards is in development</div> -->
+        <div class="flex justify-between w-96">
+            <span class="flex space-x-3">
+            <button class="btn orangeWeb solid">Start</button>
+                <span class="text-sm text-left">
+                    <p>0 Moves</p>
+                    <p>Time: 0 secs</p>
+                </span>
+            </span>
+            <button class="btn bw solid">options</button>
+        </div>
+        <div class="aspect-square w-80 sm:w-96 max-w-sm bg-orangeWeb-800 rounded-md grid grid-cols-5 grid-rows-5 gap-3 p-3">
+            {#each Array(25) as _, i}
+            <div class="w-full h-full bg-gray-900 rounded-md cursor-pointer hover:border-gray-200 hover:border-2"></div>
+            {/each}
+         
+        </div>
 </section>
