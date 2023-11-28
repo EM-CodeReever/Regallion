e2e353d64880c3775f37279be3fab360154b350e
Elliot Morrison <elliotmorrison58@gmail.com>
2023-11-28 09:45:36 -0500

    Add profile and chat room pages

---
+++ src/routes/(auth)/chat-room/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import Badge from '$components/Badge.svelte';
+    import LabelledInput from '$components/LabelledInput.svelte';
+    import type { PageData } from './$types';
+    export let data: PageData;
+    let { supabase, session,userProfile } = data;
+    let roomCode = '';
+</script>
+<svelte:head>
+    <title>Regalore - Chat Room</title>
+</svelte:head>
+<section class="w-full bg-gradient-to-b from-gray-900 to-gray-600 bg-gradient-to-r min-h-screen flex flex-col justify-center space-y-16">
+    <span class="flex flex-col items-center space-y-3 ">
+        <h1 class="text-7xl font-semibold">Regalore</h1>
+        <p >Welcome to the chat room</p>
+    </span>
+    <div class="flex flex-col justify-center items-center space-y-3 w-full">
+        <button class="btn flex-grow w-full max-w-md solid orangeWeb">Join Global Chat</button>
+        <div class="divider richBlack max-w-md">or</div>
+        <span class="grid w-full max-w-md gap-y-3 grid-cols-1">
+            <LabelledInput label="Join with room code" placeholder="Enter a room code" data={roomCode} name="CodeInput" />
+            <button class="btn flex-grow w-full max-w-md solid richBlack">Join</button>
+        </span>
+
+    </div>
+</section>
---
+++ src/routes/(auth)/chat-room/[room_code]/+page.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+    import type { PageData } from '../$types';
+    import { afterUpdate, tick } from 'svelte';
+    export let data: PageData;
+    let { supabase, session,userProfile } = data;
+    import { page } from "$app/stores";
+    import ChatMessage from '$components/ChatMessage.svelte';
+    let roomCode = $page.params.room_code;
+    let messageString = "";
+    let element: HTMLDivElement
+    let chatArray = [] as any[];
+    function leaveRoom(){
+        console.log("leave room");
+    }
+    function addMessage(message: string, self: boolean, username?: string,){
+        chatArray.push({
+            username: username ?? "username",
+            message: message,
+            self
+        });
+        messageString = "";
+        chatArray = chatArray
+    }
+    afterUpdate(() => {
+            console.log("afterUpdate");
+            if(chatArray) scrollToBottom(element);
+        });
+    
+        $: if(chatArray && element) {
+            scrollToBottom(element);
+        }
+
+    const scrollToBottom = async (node:any) => {
+        node.scroll({ top: node.scrollHeight, behavior: 'smooth' });
+    }; 
+    
+</script>
+
+<svelte:head>
+    <title>Regalore - [{roomCode}]</title>
+</svelte:head>
+
+<section class="w-full bg-gradient-to-b from-gray-900 to-gray-600 bg-gradient-to-r min-h-screen flex flex-col items-center relative">
+    <div class="h-14 mt-24 max-w-4xl w-full flex justify-between bg-[#000000aa] rounded-lg rounded-b-none py-3 items-center px-16">
+        <p>Room code: {roomCode}</p>
+        <div class="divider vertical"></div>
+        <p>Room name: {roomCode}</p>
+        <div class="divider vertical"></div>
+        <p>Memebers: 0</p>
+        <div class="divider vertical"></div>
+        <p>Online: 0</p>
+        <!-- <div class="divider vertical"></div>
+        <button class="btn danger solid  -ml-6 w-32">Leave</button> -->
+    </div>
+    <div bind:this={element} class="w-full max-w-4xl bg-[#000000aa] flex flex-col rounded-lg rounded-t-none pb-20 overflow-y-scroll pl-16 pr-14 space-y-2" style="height: 565px;" on:load={()=>{
+        scrollToBottom(element);
+    }}>
+        {#each chatArray as chatMessage}
+            <ChatMessage messageData={chatMessage} />
+        {/each}
+        
+    </div> 
+    <form class="absolute bottom-5 mx-auto flex w-full max-w-4xl px-3 items-center justify-center btn-group">
+        <input class="w-full max-w-3xl flex-grow input platinum solid rounded-r-none" placeholder="Type here . . ." bind:value={messageString} />
+        <button type="submit"  class="btn success sm solid rounded-l-none" on:click|preventDefault={()=>{
+            addMessage(messageString, true);
+        }}>send</button>
+    </form>
+
+</section>
