631a33e56fdbf1511f22b01b2b0c51292184f95c
Elliot Morrison <elliotmorrison58@gmail.com>
2023-11-28 09:44:26 -0500

    Add ChatMessage and Chatroom components

---
+++ src/components/ChatMessage.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+export let messageData: {
+    username: string | undefined | null;
+    message: string;
+    self: boolean;
+};
+</script>
+
+<p class="flex max-w-2xl select-none mt-auto {messageData.self ? 'self-end' : ''}">
+    {#if !messageData.self}
+    <span class="bg-red-600 h-fit px-1 rounded-sm mr-1 ">{messageData.username}:</span>
+    {/if}
+    <span>
+        {messageData.message}
+    </span>
+</p>
