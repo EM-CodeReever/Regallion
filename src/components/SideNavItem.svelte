2a2fe43350d244f01c83a6c1b84616ea6901140c
Elliot Morrison <elliotmorrison58@gmail.com>
2024-01-04 22:25:15 -0500

    Add SideNavItem component

---
+++ src/components/SideNavItem.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    export let isActive = false;
+    export let title = "";
+    export let linkTo = "";
+
+</script>
+
+<button class="cursor-pointer flex justify-start items-center font-semibold space-x-3 {isActive ? 'bg-gray-200' : 'hover:bg-oxfordBlue-600 hover:text-gray-300'} w-full p-3 rounded-lg relative"
+on:click={()=>{
+    goto(linkTo);
+    let element = document.getElementById('drawer-left');
+    element?.classList.remove('show');
+}}>
+    <slot />
+    <p>{title}</p>
+    {#if isActive}
+    <span class="absolute w-1 h-9 rounded-tr-lg rounded-br-lg -left-3 top-1.5 bg-gray-800"></span>
+    {/if}
+  </button>
