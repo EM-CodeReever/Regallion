596ee494ce75a11c3895cf42b829612f121acd1a
Elliot Morrison <elliotmorrison58@gmail.com>
2024-01-04 22:25:19 -0500

    Add AboutInfo SVG component

---
+++ src/components/svg/AboutInfo.svelte
@@ -0,0 +1,20 @@
+<svg
+    fill="#000000"
+    viewBox="0 0 96 96"
+    class="w-5 h-5 screen-500:hidden"
+    xmlns="http://www.w3.org/2000/svg"
+    ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    ></g><g id="SVGRepo_iconCarrier">
+        <title></title>
+        <g>
+            <path
+                d="M66,84H54V42a5.9966,5.9966,0,0,0-6-6H36a6,6,0,0,0,0,12h6V84H30a6,6,0,0,0,0,12H66a6,6,0,0,0,0-12Z"
+            ></path>
+            <path d="M48,24A12,12,0,1,0,36,12,12.0119,12.0119,0,0,0,48,24Z"
+            ></path>
+        </g>
+    </g></svg
+>
