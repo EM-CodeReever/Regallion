a67be9834806c47f9608b26e8dece854a530d79f
Elliot <elliotmorrison58@gmail.com>
2024-09-11 20:16:14 -0500

    Refactor layout component to add MainNavbar and SideNavbar components

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
     import { page } from "$app/stores";
+  import MainNavbar from "$components/MainNavbar.svelte";
+  import SideNavbar from "$components/SideNavbar.svelte";
     import SideNavItem from "$components/SideNavItem.svelte";
     import Chatroom from "$components/svg/Chatroom.svelte";
     import Home from "$components/svg/Home.svelte";
@@ -35,80 +37,12 @@
 
   </script>
 
-  <section class="flex bg-oxfordBlue-500 w-full h-fit min-h-screen p-5 screen-400:p-4 screen-400:pb-0">
-    <!-- <nav class="blur-bg fixed px-3 w-full top-0 bg-transparent flex justify-between items-center h-16 z-50">
-        <div class="flex justify-center items-center py-5 px-1">
-            <button class="btn" on:click={()=>{toggleDrawer('left')}}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#e5e5e5" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
-                  </svg> 
-            </button>             
-        </div>
-        <div class="flex space-x-2 items-center">
-          <img src="/favicon.ico" alt="Regal logo" class="w-9 h-9 hidden sm:flex">
-          <a href="/dashboard" class="bg-richBlack-200 px-2 py-1 rounded-md font-extrabold text-2xl lg:text-2xl text-gray-200">Regallion</a>
-        </div>
-        <div class="p-5">
-            <div class="dropdown bw">
-            <button class="avatar ring-1" tabindex="0">
-                <img alt="avatar" src="https://robohash.org/{userProfile?.username}" />
-            </button>
-            <div class="menu bottom-left" style="width: 20rem;">
-                <p class="subtitle text-center">Signed in as: <span class="font-bold text-success-800">
-                  {userProfile?.username}
-                </span></p>
-                <div class="is-divider" role="separator"></div>
-                <button class="item text-sm btn" tabindex="-1" on:click={()=>{goto('/')}}>Homepage</button>
-                <button class="item text-sm btn" tabindex="-1" on:click={()=>{goto('profile')}}>Profile</button>
-                <button class="item text-sm btn" tabindex="-1" on:click={()=>{goto('account-settings')}}>Account Settings</button>
-                <button class="item text-sm btn" tabindex="-1" on:click={()=>{logOut()}}>Logout</button>
-              </div>
-            </div>
-        </div>
-    </nav> -->
-    <div class="h-fit w-full flex justify-center">
-       {#if !(currentPath == "/(auth)/dashboard")}
-      <button class="btn orangeWeb absolute top-5 left-5 ghost p-2 " on:click={()=>{toggleDrawer('left')}}>
-        <Sidebar/>
-      </button> 
-      {/if}
+  <section class="flex flex-col misty-waves w-full h-fit min-h-screen p-5 screen-400:p-4 screen-400:pb-0">
+    <MainNavbar userProfile={userProfile} supabase={supabase}/>
+      <SideNavbar {currentPath}>
         <slot />
-    </div>
+        </SideNavbar>
   </section>
 
-  
-  <div data-theme="dark">
-    <label class="drawer-overlay" on:click={()=>{toggleDrawer('left')}}></label>
-    <div class="drawer left" id="drawer-left">
-      <div class="content flex flex-col h-full">
-        <label class="btn sm circle bw solid absolute right-3 top-3" on:click={()=>{toggleDrawer('left')}}>✕</label>
-        <div class="flex space-x-1 items-center m-3 mb-0">
-          <img src="/favicon.ico" alt="Regal logo" class="w-9 h-9 hidden sm:flex">
-          <a href="/dashboard" class="bg-oxfordBlue-600 px-2 py-0.5 rounded-md font-extrabold text-2xl lg:text-2xl text-gray-200">Regallion</a>
-        </div>
-        <div class="divider platinum" />
-        <SideNavItem title="Overview" isActive={false} linkTo="/dashboard">
-          <Overview/>
-        </SideNavItem>
-        <!-- <SideNavItem title="Projects" isActive={false} linkTo="/projects">
-          <Project/>
-        </SideNavItem> -->
-        <SideNavItem title="Profile" isActive={false} linkTo="/profile">
-          <Profile/>
-        </SideNavItem>
-        <SideNavItem title="Account Settings" isActive={false} linkTo="/account-settings">
-          <Settings/>
-        </SideNavItem>
-        <SideNavItem title="Homepage" isActive={false} linkTo="/">
-          <Home/>
-        </SideNavItem>
-        <button class="cursor-pointer flex justify-start items-center font-semibold space-x-3 hover:bg-oxfordBlue-600 hover:text-gray-300 w-full p-3 rounded-lg relative"
-                on:click={()=>{
-                    logOut();
-                }}>
-                <Logout />
-                <p>Logout</p>
-            </button>
-      </div>
-    </div>
-  </div>
+  <!-- <RiddleBox/> -->
+ 
