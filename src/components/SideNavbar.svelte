61189370c39e3926320a99f95d4026ba1b3124c1
Elliot <elliotmorrison58@gmail.com>
2024-09-11 20:15:54 -0500

    Add SideNavbar component for displaying the side navigation bar

---
+++ src/components/SideNavbar.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { Club, DiamondPercent, House, MessageSquareMore } from "lucide-svelte";
+
+
+  import SideNavItem from "./SideNavItem.svelte";
+  import Project from "./svg/Project.svelte";
+
+  export let currentPath: string | null;
+
+  let staticPath = "/(auth)";
+
+</script>
+
+
+<div class="w-full h-fit flex flex-col space-y-5 mt-4 lg:space-x-5 ping-pong-breakpoint:space-y-0 ping-pong-breakpoint:flex-row">
+    <div class="w-full max-w-[14rem] text-gray-200 p-3 h-fit rounded-xl justify-start items-center space-y-3 flex-col hidden lg:flex">
+
+        <SideNavItem title="Dashboard" isActive={(currentPath === staticPath + "/dashboard")} linkTo="/dashboard">
+            <Project/>
+        </SideNavItem>
+        <SideNavItem title="Ping Pong" isActive={(currentPath === staticPath + "/ping-pong")} linkTo="/ping-pong">
+          <DiamondPercent/>
+        </SideNavItem>
+        <SideNavItem title="Memory Cards" isActive={(currentPath === staticPath + "/memory-card")} linkTo="/memory-card">
+            <Club/>
+        </SideNavItem>
+        <SideNavItem title="Regalore" isActive={(currentPath === staticPath + "/chat-room")} linkTo="/chat-room">
+          <MessageSquareMore/>
+        </SideNavItem>
+        <SideNavItem title="Homepage" isActive={false} linkTo="/">
+          <House/>
+        </SideNavItem>
+      </div>
+      <div class="w-full ">
+        <slot />
+      </div>
+</div>
