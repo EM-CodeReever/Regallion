89edcbe23fc34ff56dc3203fe8ace6d9b4f486f9
Elliot Morrison <elliotmorrison58@gmail.com>
2026-05-28 16:07:12 -0500

    node runtime plus health

--- svelte.config.js
+++ svelte.config.js
@@ -20,7 +20,7 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			runtime: 'nodejs24.15.0',
+      		runtime: 'nodejs24.x'
 		})
 	}
 };
